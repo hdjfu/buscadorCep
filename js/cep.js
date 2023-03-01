0df122dbe360ce147fd507ce0304befff09e7741
hdjfu <hdjfulucas@gmail.com>
2023-03-01 14:22:24 -0300

    first commit

---
+++ js/cep.js
@@ -0,0 +1,38 @@
+function buscaCep(){
+    let cep = document.getElementById('txtCep').value;
+    if(cep !== ""){
+        let url = "https://brasilapi.com.br/api/cep/v1/" + cep;
+        
+        let req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.send();
+
+        //Tratamenro da requisição
+        req.onload = function() {
+            if(req.status === 200){
+                let endereco = JSON.parse(req.response);
+                document.getElementById("txtRua").value = endereco.street;
+                document.getElementById("txtBairro").value = endereco.neighborhood;
+                document.getElementById("txtCidade").value = endereco.city;
+                document.getElementById("txtEstado").value = endereco.state;
+            }
+            else if(req.status == 404){
+                alert("CEP inválido");
+            }else{
+                alert("Erro ao fazer requisição");
+            }
+        }
+    }
+}
+
+window.onload = function(){
+    let txtCep = document.getElementById("txtCep");
+    txtCep.addEventListener("blur", buscaCep);
+    
+
+    txtCep.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            buscaCep();
+        }
+    });
+}
